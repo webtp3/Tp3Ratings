e804ef912b3582a8a406c904939204100b8956d5
tudy <email@thomasruta.de>
2018-09-06 16:23:58 +0200

    [fix] fix base + /

--- ext_emconf.php
+++ ext_emconf.php
@@ -20,7 +20,7 @@ $EM_CONF[$_EXTKEY] = array (
   'uploadfolder' => false,
   'createDirs' => '',
   'clearCacheOnLoad' => 0,
-  'version' => '1.2.8',
+  'version' => '1.2.9',
   'constraints' => 
   array (
     'depends' => 
